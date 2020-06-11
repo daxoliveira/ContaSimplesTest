d36951af595fc6f63bb58fb065f6f25786407e98
daxoliveira <dax-oliveira-br@gmail.com>
2020-06-11 17:34:03 -0300

    Create basic main components from wireframe

--- src/App.js
+++ src/App.js
@@ -5,20 +5,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          <img src={logo} className="logo" alt="logo" />
+          <h4 className="greeting">Bom dia!</h4>
+          <img src="" alt="user-avatar" className="avatar"/>
+          <h4 className="user-name">Dax Oliveira</h4>
+          <button></button>
+
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Minha Conta</li>
+              <li>Cartão de Crédito</li>
+              <li>Gestão de Cobranças</li>
+              <li>Benefícios</li>
+              <li>Tarifas</li>
+              <li>Drop-down</li>
+            </ul>
+          </nav>        
       </header>
+
+      <body className="App-body">
+        <h4>Seu rendimento desde a abertura da sua Conta Simples!</h4>
+        <img src="" alt="graph of investment profits"/>
+        <footer>
+          <h4>Perguntas Frequents</h4>
+          <h4>Converse com a gente!</h4>
+        </footer>
+      </body>
+
     </div>
   );
 }
