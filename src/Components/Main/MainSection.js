6ea4366e6ca195cedb878c80564b17451374a615
daxoliveira <dax-oliveira-br@gmail.com>
2020-06-14 16:52:55 -0300

    Create navigation bar second version

---
+++ src/Components/Main/MainSection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class MainSection extends Component {
+    render () {
+        return (
+            <div>MainSection</div>
+        )
+    }
+}
+
+export default MainSection
---
+++ src/Components/Nav/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import Burger from './Burger';
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 55px;
+  border-bottom: 2px solid #f1f1f1;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  .logo {
+    padding: 15px 0;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+    }
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <div className="logo">
+        Nav Bar
+      </div>
+      <Burger />
+    </Nav>
+  )
+}
+
+export default Navbar
