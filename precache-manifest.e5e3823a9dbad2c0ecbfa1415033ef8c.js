7068f7ed1d36a80ca7ba4c62b5b210cf3c400675
daxoliveira <dax-oliveira-br@gmail.com>
2020-06-21 22:57:51 -0300

    Updates

---
+++ precache-manifest.e5e3823a9dbad2c0ecbfa1415033ef8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35bf42328c885196781556dccfcc0d4",
+    "url": "/ContaSimplesTest/index.html"
+  },
+  {
+    "revision": "bf8883098272a7e390a4",
+    "url": "/ContaSimplesTest/static/css/main.482e58a0.chunk.css"
+  },
+  {
+    "revision": "d7edad4d1cac2bc376bd",
+    "url": "/ContaSimplesTest/static/js/2.7d099c65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ContaSimplesTest/static/js/2.7d099c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8883098272a7e390a4",
+    "url": "/ContaSimplesTest/static/js/main.e97681d5.chunk.js"
+  },
+  {
+    "revision": "1d5fa0cde8fc2e2f2d79",
+    "url": "/ContaSimplesTest/static/js/runtime-main.4cd01688.js"
+  },
+  {
+    "revision": "4f59c0d98968fd24def9008084534bfd",
+    "url": "/ContaSimplesTest/static/media/logo.4f59c0d9.svg"
+  }
+]);
